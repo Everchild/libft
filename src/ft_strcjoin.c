a2a3c10310e54488b45ff520186c389e1c697261
Sarah BROCHARD <sbrochar@e3r4p7.42.fr>
2017-06-05 19:42:02 +0200

    fixing leaks in strcjoin

--- src/ft_strcjoin.c
+++ src/ft_strcjoin.c
@@ -6,7 +6,7 @@
 /*   By: sbrochar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/12 19:31:17 by sbrochar          #+#    #+#             */
-/*   Updated: 2016/04/12 19:31:18 by sbrochar         ###   ########.fr       */
+/*   Updated: 2017/06/05 19:37:07 by sbrochar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ char				*ft_strcjoin(char const *s1, char const *s2, char c)
 	if (s1 && s2)
 	{
 		ret = ft_strjoin(s1, &c);
-		ret = ft_strjoin(ret, s2);
+		ret = ft_strjoinf(ret, (char *)s2, 1);
 		return (ret);
 	}
 	return (NULL);
