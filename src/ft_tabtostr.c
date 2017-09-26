e089802660c77733cd6b1a50e754fbec71c7d28a
Sarah BROCHARD <sbrochar@e3r3p21.42.fr>
2017-09-26 15:14:48 +0200

    fixing tabtostr

--- src/ft_tabtostr.c
+++ src/ft_tabtostr.c
@@ -6,7 +6,7 @@
 /*   By: sbrochar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/26 13:39:05 by sbrochar          #+#    #+#             */
-/*   Updated: 2017/09/26 14:38:37 by sbrochar         ###   ########.fr       */
+/*   Updated: 2017/09/26 15:12:56 by sbrochar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,23 +15,27 @@
 size_t				get_size(char **tab)
 {
 	size_t			ret;
+	size_t			nb_str;
    
 	ret = 0;
+	nb_str = 0;
 	if (tab)
 	{
 		while (*tab)
 		{
 			ret += ft_strlen(*tab);
 			tab++;
+			nb_str++;
 		}
 	}
-	return (ret);
+	return (ret + nb_str - 1);
 }
 
 char				*ft_tabtostr(char **tab)
 {
 	char			*ret;
 	size_t			size;
+	char			*tmp;
 
 	size = get_size(tab);
 	ret = ft_strnew(size);
@@ -40,6 +44,8 @@ char				*ft_tabtostr(char **tab)
 		while (*tab)
 		{
 			ft_strcat(ret, *tab);
+			tmp = ft_strchr(ret, 0);
+			tmp[0] = ' ';
 			tab++;
 		}
 		return (ret);
