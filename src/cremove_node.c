65b3c09fe44a64a1087abff6ffb163b7bfaf547d
Sarah BROCHARD <sbrochar@e3r10p4.42.fr>
2017-10-18 05:18:37 +0200

    fixing cremove_node

--- src/cremove_node.c
+++ src/cremove_node.c
@@ -6,7 +6,7 @@
 /*   By: sbrochar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/12 19:36:01 by sbrochar          #+#    #+#             */
-/*   Updated: 2017/10/05 14:16:43 by sbrochar         ###   ########.fr       */
+/*   Updated: 2017/10/18 05:18:23 by sbrochar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,20 @@ void				cremove_node(t_clist **list, t_node *node)
 {
 	if (list && *list && node)
 	{
-		if (node->prev)
+		if ((*list)->nb_nodes > 1)
+		{
+			if (node == (*list)->start)
+				(*list)->start = node->next;
+			if (node == (*list)->end)
+				(*list)->end = node->prev;
 			node->prev->next = node->next;
-		if (node->next)
 			node->next->prev = node->prev;
-		if (node == (*list)->start)
-			(*list)->start = node->next;
-		if (node == (*list)->end)
-			(*list)->end = node->prev;
+		}
+		else
+		{
+			(*list)->start = NULL;
+			(*list)->end = NULL;
+		}
 		ft_memdel((void **)&(node->content));
 		ft_memdel((void **)&node);
 		(*list)->nb_nodes -= 1;
